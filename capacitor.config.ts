382516969b8246a2bc62d2040db3c88bca684191
IvanJCCorreia <ivanjacobcorreia@gmail.com>
2023-04-03 14:36:14 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'GContGApp',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
